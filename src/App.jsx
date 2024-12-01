dcd8f61f83b111bcd24b99a06fc60b77226cfbde
Kowsalya Srinivasan <kowsisrini25@gmail.com>
2024-12-01 15:15:11 +0530

    Dev Connect - react router, outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import NavBar from "./NavBar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Body from "./Body";
+import Login from "./Login";
+import Profile from "./Profile";
 
 function App() {
   return (
     <>
-      <NavBar />
-      <h1 className="text-3xl font-bold underline">Dev Connect</h1>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+
+      {/* <NavBar />
+      <h1 className="text-3xl font-bold underline">Dev Connect</h1> */}
     </>
   );
 }
