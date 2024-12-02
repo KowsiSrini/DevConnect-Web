b59f7161fe6d387007d2460c476e1e380d6ca821
Kowsalya Srinivasan <kowsisrini25@gmail.com>
2024-12-02 20:02:34 +0530

    Dev Connect - Login screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-import Body from "./Body";
-import Login from "./Login";
-import Profile from "./Profile";
+import { Provider } from "react-redux";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import Feed from "./components/Feed";
+import appStore from "./utils/appstore";
 
 function App() {
   return (
     <>
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/" element={<Body />}>
-            <Route path="/login" element={<Login />} />
-            <Route path="/profile" element={<Profile />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-
-      {/* <NavBar />
-      <h1 className="text-3xl font-bold underline">Dev Connect</h1> */}
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/" element={<Feed />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Footer = () => {
   return (
     <footer className="footer bg-base-300 text-neutral-content items-center p-4 fixed bottom-0">
